cb8e4a10050e6fb95eed8f326e187bf6c3b68032
Rachel Townson <townsor@shure.com>
2024-10-21 21:46:12 -0500

    floating nav bar

---
+++ src/app/header.tsx
@@ -0,0 +1,20 @@
+import '@/app/globals.css';
+import React from 'react';
+
+export function NavBar() {
+    const button_style = 'text-lg px-5';
+
+    return (
+        <nav className='font-neuton sticky top-0 px-5 py-5'>
+            <div className='flow-root'>
+                <h1 className='text-lg float-left'>Rachel Townson</h1>
+                <div className='float-right'>
+                    <button className={`${button_style}`}> CAREER </button>
+                    <button className={`${button_style}`}> PROJECTS </button>
+                    <button className={`${button_style}`}> CONTACT </button>
+                </div>
+                
+            </div>
+        </nav>
+      );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,56 @@
 import '@/app/globals.css';
+import { NavBar } from './header';
+
 
 export default function Home() {
   return (
-    <body className='font-catamaran'>
-      <div className='max-w-4xl mx-auto my-20'>
-        <div className='grid grid-cols-3'>
-          <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
-            <h1 className='font-neuton text-xl'>Rachel Townson</h1>
-            <p className='text-sm'>Website coming soon...</p>
-          </div>
-          <div className='bg-midtone rounded-lg p-3 m-3'>
-            <p> <a href='mailto:townsonr@msu.edu'>my email</a></p>
-            <p> <a href='https://www.linkedin.com/in/rachel-townson/' target="_blank" rel="noopener noreferrer">my linkedin</a></p>
-            <p> <a href='https://github.com/townsonr' target="_blank" rel="noopener noreferrer">my github</a> </p>
+      <div>
+        <NavBar />
+        <div className='max-w-4xl mx-auto my-20'>
+          <div className='grid grid-cols-3'>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3'>
+              <p> <a href='mailto:townsonr@msu.edu'>my email</a></p>
+              <p> <a href='https://www.linkedin.com/in/rachel-townson/' target="_blank" rel="noopener noreferrer">my linkedin</a></p>
+              <p> <a href='https://github.com/townsonr' target="_blank" rel="noopener noreferrer">my github</a> </p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
+            <div className='bg-midtone rounded-lg p-3 m-3 col-span-2'>
+              <h1 className='font-neuton text-xl'>Rachel Townson</h1>
+              <p className='text-sm'>Website coming soon...</p>
+            </div>
           </div>
         </div>
       </div>
-    </body>
   );
 }
