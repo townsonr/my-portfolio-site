c480e42ba7c2de9e0f80c3534d51e18efc3d4e6b
townsonr <racheltownson24@gmail.com>
2025-06-23 21:54:40 -0500

    typo

--- src/app/home/bio_card.tsx
+++ src/app/home/bio_card.tsx
@@ -4,7 +4,7 @@ export default function BioCard() {
     return (
         <TextBox>
             <p>I currently work at Shure Incorporated as a test automation engineer. 
-                I work primarily with professional audio and wireless product, creating test programs 
+                I work primarily with professional audio and wireless products, creating test programs 
                 to verify software, firmware, and hardware quality. </p>
             <p>I build software that inspires creative people to make amazing things! I’m interested 
                 in computer graphics, audio processing, and other types of media-related software.</p>
